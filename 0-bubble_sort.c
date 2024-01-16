32ca2df38f2894111ac0d5222905cabdcb9c34de
Fizzyfog <idrissisarah@gmail.com>
2024-01-16 09:12:13 +0100

    Add comments and flag to track swapping in bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,16 +10,17 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t current_index, unsorted_size = size;
 	int temporary_value;
-
+	/* Flag to track if any swapping occurred */
 	bool has_swapped = false;
-
+	/* Return if array is NULL or has less than 2 elements */
 	if (array == NULL || size < 2)
 		return;
 
 	do {
-
+		/* Iterate over each element in the unsorted part of the array */
 		for (current_index = 0; current_index < unsorted_size - 1; current_index++)
 		{
+			/* If current element is greater than the next one, swap them */
 			if (array[current_index] > array[current_index + 1])
 			{
 				temporary_value = array[current_index];
